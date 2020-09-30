bf2f99ad3d7d70db794e570fa28074ab76018dba
Martin <liman.martinn@gmail.com>
2020-09-30 14:28:34 +0700

    add email and test suite

---
+++ Test Suites/Item detail test.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Item detail test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>72c16583-90cf-470b-afd9-b919b3c6be53</testSuiteGuid>
+   <testCaseLink>
+      <guid>806d42b0-9799-47f3-9792-be6ede668996</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>119da7b3-bc56-4ddc-ac06-eaf8f4009bdb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8d65427-8278-4195-b1c2-37cbb938d420</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b83962fd-9593-4afc-8ab3-c633f76ba577</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0a19d8be-ae56-40c4-a93f-506d95d8c0e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_005</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b54523af-7340-461a-b2a9-8b422c77eb7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_006</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>012944e5-cc49-4cad-a2ed-a0601b7bbea8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_007</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0bed342c-58e1-4f9e-9ff7-afe3dfc2669d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_008</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1dcbcb6c-60a9-46f6-8f0c-ab9bdf085d99</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_009</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>27f72d2b-0a3b-43ec-88d0-b531e4fab5c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_010</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b2f6d183-dd79-4297-9d59-6236a24f6ffa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_011</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c17bdfa6-6c22-429e-8d06-8957912a7f9d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_012</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7c2aad60-a5b1-40af-a38c-3e8633a88521</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_013</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04beaa7d-7fad-458c-bc61-30b1054e1fd0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_014</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1dd9d597-118a-4588-b90a-fce6651850ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_015</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>14fb77db-4ae6-4f57-a4ca-4b641909fc89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_016</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e1bf8e06-3eaa-4c55-b628-4cbbe6774591</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_017</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc8f391b-3c63-4bba-9eef-36eb1abbbf96</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Item Details/TEST CASE/TC_DETAILS_UI_018</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
